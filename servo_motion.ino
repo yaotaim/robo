3132cec2b0838cf298d23bb285b242c77594d664
yaotaim <tatananakaka369@gmail.com>
2024-07-09 11:47:03 +0900

    Update servo_motion.ino

--- servo_motion.ino
+++ servo_motion.ino
@@ -13,7 +13,6 @@ void nameten();
 void runrun();
 void nani();
 
-// タイミング用の変数
 unsigned long lastTime = 0;
 unsigned long interval = 0;
 
@@ -22,40 +21,35 @@ void setup() {
   myservo6.attach(6);  
   myservo5.attach(5);  
   myservo4.attach(4); 
-
-  // シリアル通信開始
   Serial.begin(9600);
 }
 
 void loop() {
-  // 現在の時間を取得
   unsigned long currentTime = millis();
 
-  // シリアル入力の処理
   if (Serial.available() > 0) {
     val = Serial.read();
     if (val == 'a') {
       nameten();
-      interval = 1000; // 1秒の間隔で実行
+      interval = 1000; 
     } else if (val == 's') {
       runrun();
-      interval = 10000; // 10秒の間隔で実行
+      interval = 10000; 
     } else if (val == 'd') {
       nani();
-      interval = 10000; // 10秒の間隔で実行
+      interval = 10000;
     }
-    // 最後の実行時間を更新
     lastTime = currentTime;
   }
 
-  // タイミングに従ってサーボを制御
+ 
   if (currentTime - lastTime >= interval) {
     // タイミングに達したらここに入る
-    // ここでサーボの制御を行う
+    //未作成
   }
 }
 
-// 以下、サーボ制御関数は先ほどと同じ
+
 void nameten(){
   myservo7.write(90, 70, true);
   myservo6.write(90, 70, true);
