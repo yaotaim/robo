36efd6047dccc793a8307dea530b5d60ec01a107
yaotaim <tatananakaka369@gmail.com>
2024-07-09 14:19:08 +0900

    Update GP2Y_ar.ino

--- arduino/GP2Y_ar.ino
+++ arduino/GP2Y_ar.ino
@@ -1,20 +1,24 @@
 //arduinoでシャープの距離センサーを使う
-void setup()
-{
-	Serial.begin(115200);
-}
+float Vcc = 5.0;//電源電圧
+float distance_1;
+float distance_2;
+const int ANALOG = 0;//センサ信号入力
+int ledPin = 50;
 
-void loop()
-{
-	int value = analogRead(8);
-	int distance = (6787/(value-3))-4;
-	//50cm以下で反応
-	if(distance <= 100){
-		Serial.print(distance);
-		Serial.println("cm");
-	}
-	else{
-		Serial.println("not found");
-	}
-	delay(500);
-}
+void setup(){
+  pinMode(ledPin, OUTPUT);
+ Serial.begin(9600);//シリアル通信
+  }
+
+void loop(){ 
+ distance_1 = Vcc*analogRead(ANALOG)/1023; //(5.0V*センサ数値/1023)1023は5V入力時の値
+ distance_2 = 26.549*pow(distance_1,-1.2091); //距離換算
+ Serial.println(distance_2);
+ if (distance_2<15){
+  digitalWrite(ledPin, HIGH);  
+  }
+  else{
+    digitalWrite(ledPin, LOW);  
+  }
+ delay(100); 
+ } 
