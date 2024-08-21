be26bd8d75fdacff3907ce3fc8a9f0aa32eb4b95
yaotaim <tatananakaka369@gmail.com>
2024-08-21 15:08:37 +0900

    Update DC_ESP.ino

--- ESP/DC_ESP.ino
+++ ESP/DC_ESP.ino
@@ -1,11 +1,8 @@
-//ESPでシリアル入力してDCモーターを操作(L298N)
-
-
-//シリアル入力してDCモーターを操作(L298N)
+// ESPでシリアル入力してDCモーターを操作(L298N)
 
 // モーターAのピン
 int ENA = 33;
-int IN1 = 25;
+int IN1 = 32;
 int IN2 = 26;
 
 // モーターBのピン
@@ -18,7 +15,7 @@ const int freq = 30000;
 const int pwmChannelA = 0;
 const int pwmChannelB = 1;
 const int resolution = 8;
-int dutyCycle = 255;
+int dutyCycle = 255;  // 初期速度
 
 void setup() {
   // ピンを出力として設定
@@ -40,7 +37,7 @@ void setup() {
   Serial.begin(115200);
 
   // 初期メッセージ
-  Serial.println("モーター制御の準備完了。1: 前進, 2: 後退, 3: 停止");
+  Serial.println("モーター制御の準備完了。1: 前進, 2: 後退, 3: 停止, 4: 右回り, 5: 左回り, 速度調整: 0-9");
 }
 
 void loop() {
@@ -48,7 +45,7 @@ void loop() {
     char command = Serial.read();
 
     switch (command) {
-      case '1':
+      case 'w':
         Serial.println("前進");
         // モーターA前進
         digitalWrite(IN1, HIGH);
@@ -60,7 +57,7 @@ void loop() {
         ledcWrite(pwmChannelB, dutyCycle);
         break;
 
-      case '2':
+      case 'x':
         Serial.println("後退");
         // モーターA後退
         digitalWrite(IN1, LOW);
@@ -72,7 +69,7 @@ void loop() {
         ledcWrite(pwmChannelB, dutyCycle);
         break;
 
-      case '3':
+      case 's':
         Serial.println("停止");
         // モーターA停止
         digitalWrite(IN1, LOW);
@@ -84,9 +81,35 @@ void loop() {
         ledcWrite(pwmChannelB, 0);
         break;
 
+      case 'e':
+        Serial.println("右回り");
+        digitalWrite(IN1, HIGH);
+        digitalWrite(IN2, LOW);
+        ledcWrite(pwmChannelA, dutyCycle);
+        digitalWrite(IN3, LOW);
+        digitalWrite(IN4, HIGH);
+        ledcWrite(pwmChannelB, dutyCycle);
+        break;
+
+      case 'q':
+        Serial.println("左回り");
+        digitalWrite(IN1, LOW);
+        digitalWrite(IN2, HIGH);
+        ledcWrite(pwmChannelA, dutyCycle);
+        digitalWrite(IN3, HIGH);
+        digitalWrite(IN4, LOW);
+        ledcWrite(pwmChannelB, dutyCycle);
+        break;
+      
+      // 速度調整（0-9のキー入力によりデューティサイクルを変更）
+      case '0' ... '9':
+        dutyCycle = map(command - '0', 0, 9, 0, 255);  // 0から9を0から255の範囲にマップ
+        Serial.print("速度調整: ");
+        Serial.println(dutyCycle);
+        break;
+
       default:
         Serial.println("無効なコマンド");
     }
   }
 }
-        
