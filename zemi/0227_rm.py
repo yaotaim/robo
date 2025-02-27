f6b734b98df7c5f8ed27716db1140c38cd097e4c
yaotaim <tatananakaka369@gmail.com>
2025-02-27 12:26:23 +0900

    Update 0227_rm.py

--- zemi/0227_rm.py
+++ zemi/0227_rm.py
@@ -4,6 +4,3 @@ ChatGPT: エラーが発生しました: Error code: 401 - {'error': {'message':
 You: hello
 ChatGPT: エラーが発生しました: Error code: 401 - {'error': {'message': 'Incorrect API key provided: sk-proj-********************************************************************************************************************************************************DMwA. You can find your API key at https://platform.openai.com/account/api-keys.', 'type': 'invalid_request_error', 'param': None, 'code': 'invalid_api_key'}}
 You: exit
-
-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
-SMC:SR00013690,E0i1j2i1aa
