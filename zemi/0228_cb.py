9a9c15a9ddbc51a88a6f7a84623ed855e5cf6ff2
yaotaim <tatananakaka369@gmail.com>
2025-02-28 14:22:27 +0900

    Update 0228_cb.py

--- zemi/0228_cb.py
+++ zemi/0228_cb.py
@@ -1,19 +1,8 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 
-bot = ChatBot("チャットボット試作版")
-
-training_ = open('chat.txt', 'r').readlines()
-training2_ = open('chat2.txt', 'r').readlines()
+bot = ChatBot("チャットボット試作版", storage_adapter="chatterbot.storage.SQLStorageAdapter", database_uri="sqlite:///tanaka.sqlite3")
 
 trainer = ListTrainer(bot)
-
+training_ = ["こんにちは", "こんにちは！", "元気ですか？", "元気です！"]
 trainer.train(training_)
-trainer.train(training2_)
-
-while True:
-    try:
-        bot_input = bot.get_response(input())
-        print(bot_input)
-    except(KeyboardInterrupt, EOFError, SystemExit):
-        break
